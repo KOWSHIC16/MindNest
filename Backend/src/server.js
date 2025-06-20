a390954597d45c9e47cf0f4c6eb61031dc08042f
@KOWSHIC16 <kowshican1610@gmail.com>
2025-06-20 14:47:32 +0530

    Update dependencies and project files

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,30 +1,47 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import { connectDB } from './config/db.js';
-import notesRoutes from './routes/notesRoutes.js';
-import rateLimiter from './middlewares/rateLimiter.js';
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middlewares/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors(
-    {
-        origin: "http://localhost:5173",                       // Allow requests from the frontend
-    }
-));
-app.use(express.json());                                        // Middleware to parse JSON bodies
-app.use(rateLimiter);                                           // Apply rate limiting middleware
+// middleware
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-app.use('/api/notes/', notesRoutes);
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
+
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/vite-project/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Frontend/vite-project", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log('Server is running on port:', PORT);
+        console.log("Server started on PORT:", PORT);
     });
 });
-
-
